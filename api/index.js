5f19e86dd85debb102919526413211b8cb323fba
bdumbleton <64277538+bdumbleton@users.noreply.github.com>
2021-04-16 09:56:02 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
-// 👉 Replace this with express-oauth2-bearer require 👈
+const { auth } = require("express-oauth2-bearer");
 const {
   checkUrl,
   APP_URL, // Public URL for this app
@@ -47,7 +47,7 @@ app.get("/total", (req, res) => {
   res.send({ total, count: expenses.length });
 });
 
-// 👉 Replace this with auth middleware 👈
+app.use(auth());
 
 app.get("/reports", (req, res) => {
   res.send(expenses);
